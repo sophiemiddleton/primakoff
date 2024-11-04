ecc03dab6cebe33e8326c1f2b82106db25fc837c
Nikita Blinov <nblinov@gmail.com>
2024-11-03 20:41:35 -0500

    Added vertex information to LHE output

--- lhe_output.py
+++ lhe_output.py
@@ -29,15 +29,16 @@ def create_xml_event(event, ma, mN):
     evt_info = " {num_part} {proc_id} {weight} {scale} {qed_coupling} {qcd_coupling}\n".format(num_part=6,proc_id=2,weight=1,scale=-1,qed_coupling=1./137.,qcd_coupling=0.1081)
     photon = particle_string(PID=22, status = -1, mother1 =0,mother2 =0,color1=0,color2=0,px=event[0,1],py=event[0,2],pz=event[0,3],E=event[0,0], mass=0,vtim=0,helicity=0) + '\n'
     nucleus = particle_string(PID=623, status = -1, mother1 =0,mother2 =0,color1=0,color2=0,px=0,py=0,pz=0,E=mN, mass=mN,vtim=0,helicity=0)+ '\n'
-
+    dv = event[5] # ALP decay vertex
     # use vtim field to store the decay time in cm of the ALP. Use this to reconstruct the displaced vertex 
-    ALP = particle_string(PID=666, status = 2, mother1 =1,mother2 =2,color1=0,color2=0,px=event[1,1],py=event[1,2],pz=event[1,3],E=event[1,0], mass=ma,vtim=event[5][0],helicity=0)+ '\n'
+    ALP = particle_string(PID=666, status = 2, mother1 =1,mother2 =2,color1=0,color2=0,px=event[1,1],py=event[1,2],pz=event[1,3],E=event[1,0], mass=ma,vtim=dv[0],helicity=0)+ '\n'
     nucleus_prime = particle_string(PID=623, status = 1, mother1 =0,mother2 =0,color1=0,color2=0,px=event[2,1],py=event[2,2],pz=event[2,3],E=event[2,0], mass=mN,vtim=0,helicity=0)+ '\n'
 
     daughter1 = particle_string(PID=22, status = 1, mother1 =3,mother2 =0,color1=0,color2=0,px=event[3,1],py=event[3,2],pz=event[3,3],E=event[3,0], mass=0,vtim=0,helicity=0) + '\n'
     daughter2 = particle_string(PID=22, status = 1, mother1 =3,mother2 =0,color1=0,color2=0,px=event[4,1],py=event[4,2],pz=event[4,3],E=event[4,0], mass=0,vtim=0,helicity=0) + '\n'
+    decay_vertex = '#vertex ' + str(dv[1]) + ' ' + str(dv[2]) + ' ' + str(dv[3]) + ' [' + str(dv[0]) + '] ' + '\n'
 
-    event_string = "<event>\n" + evt_info + photon + nucleus + ALP + nucleus_prime + daughter1 + daughter2 + "</event>"
+    event_string = "<event>\n" + evt_info + photon + nucleus + ALP + nucleus_prime + daughter1 + daughter2 + decay_vertex + "</event>"
 
     return event_string
 
